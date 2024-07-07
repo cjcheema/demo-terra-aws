f7f102674a0618206e972a185a49a9583d2554c4
cjcheema <cheema.cj@gmail.com>
2024-07-07 17:10:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "ap-south-1"  # Provide your desire AWS region here
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Replace with a valid AMI ID
-  instance_type = "t2.micro"
+  ami           = "ami-03100b7790806a77e"  # Replace with a valid AMI ID
+  instance_type = "t2.micro"  # Provide the desire AWS instance type
 
   tags = {
-    Name = "ExampleInstance"
+    Name = "cj-demo-linux" # Provide identical instance name here
   }
 }
