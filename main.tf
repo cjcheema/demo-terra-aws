a38b724511767b24bd901f204149aea746a42f56
charanjit-dev <charanjit.singh@outlook.in>
2025-04-26 01:18:47 +0530

    provider and ec2 key pair information added

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.57.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "ap-south-1"  # Provide your desire AWS region here
-}
-
 resource "aws_instance" "cj-vm" {
   ami           = "ami-01376101673c89611"  # Replace with a valid AMI ID
   instance_type = "t2.micro"  # Provide the desire AWS instance type
+  key_name = "cjkeypair" # Provide the name of your key pair here
 
   tags = {
     Name = "cj-demo-linux" # Provide identical instance name here
